19dbe3f46d64ebc2227237eb4ccd88f781f2becd
zhivko <klemen.zivkovic@gmail.com>
2018-11-14 11:49:39 +0100

    removed build files

--- build/AsyncPing/component_project_vars.mk
+++
@@ -1,7 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/AsyncPing/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/AsyncPing -lAsyncPing
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += AsyncPing
-component-AsyncPing-build: 
