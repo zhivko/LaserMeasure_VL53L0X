0c46f8e67870a01e287eedbc91800bee4396a566
zhivko <klemen.zivkovic@gmail.com>
2019-01-14 16:16:27 +0100

    latest

--- build/arduinoWebSockets/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/arduinoWebSockets/src
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/arduinoWebSockets -larduinoWebSockets
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += arduinoWebSockets
-COMPONENT_LDFRAGMENTS += 
-component-arduinoWebSockets-build: 
