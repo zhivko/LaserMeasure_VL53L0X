57e8c89228352cb3ff73134baa92d26348c468ae
zhivko <klemen.zivkovic@gmail.com>
2019-04-04 20:39:38 +0100

    removed build folder

--- build/AsyncTCP/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/AsyncTCP/src
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/AsyncTCP -lAsyncTCP
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += AsyncTCP
-COMPONENT_LDFRAGMENTS += 
-component-AsyncTCP-build: 
